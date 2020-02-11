05585b2c3d465bdf5b6b9b21a51b6c9e1422b20b
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-02-11 18:03:15 +0100

    added form values

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { getAuthorsQuery} from '../queries/queries'
 
 function AddBook() {
+
+    const [name, setName] = useState('')
+    const [genre, setGenre] = useState('')
+    const [author, setAuthor] = useState('')
+
     const { loading, error, data } = useQuery(getAuthorsQuery);
     if (loading) return <div>Loading</div>
 
@@ -10,20 +15,21 @@ function AddBook() {
 
     const handleSubmit = e => {
         e.preventDefault()
+        console.log({name,genre, author})
     }
     const formular = (
         <form onSubmit={handleSubmit}>
             <div>
                 <label>Book Name:</label>
-                <input type="text"/>
+                <input type="text" onChange={(e) => setName(e.target.value)}/>
             </div>
             <div>
                 <label>Genre:</label>
-                <input type="text"/>
+                <input type="text" onChange={(e) => setGenre(e.target.value)}/>
             </div>
             <div>
                 <label>Author</label>
-                <select>
+                <select onChange={(e) => setAuthor(e.target.value)}>
                     <option>Select Author</option>
                     {authors.map(author => <option key={author.id} value={author.id}>{author.name}</option>)}
                 </select>
