9bd282d22f93df7c9a33591028377b66a86309fe
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-02-11 16:22:52 +0100

    added front end

---
+++ client/src/components/BookList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BookList() {
+    return (
+        <div>
+            <ul>
+                <li>Book Name</li>
+            </ul>
+        </div>
+    )
+}
