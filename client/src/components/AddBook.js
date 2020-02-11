1e85365b40ef7912d97843bab017234d93ff00bd
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-11 22:47:39 +0100

    added addbok mutation

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,43 +1,51 @@
 import React, { useState } from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { getAuthorsQuery} from '../queries/queries'
+import { useQuery, useMutation } from '@apollo/react-hooks'
+import { getAuthorsQuery, getBooksQuery } from '../queries/queries'
+import { ADD_BOOK } from '../mutations/mutations'
 
 function AddBook() {
+  const [name, setName] = useState('')
+  const [genre, setGenre] = useState('')
+  const [author, setAuthor] = useState('')
+  const [addBook] = useMutation(ADD_BOOK)
 
-    const [name, setName] = useState('')
-    const [genre, setGenre] = useState('')
-    const [author, setAuthor] = useState('')
+  const { loading, error, data } = useQuery(getAuthorsQuery)
+  if (loading) return <div>Loading</div>
 
-    const { loading, error, data } = useQuery(getAuthorsQuery);
-    if (loading) return <div>Loading</div>
+  const { authors } = data
 
-    const { authors } = data
-
-    const handleSubmit = e => {
-        e.preventDefault()
-        console.log({name,genre, author})
-    }
-    const formular = (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Book Name:</label>
-                <input type="text" onChange={(e) => setName(e.target.value)}/>
-            </div>
-            <div>
-                <label>Genre:</label>
-                <input type="text" onChange={(e) => setGenre(e.target.value)}/>
-            </div>
-            <div>
-                <label>Author</label>
-                <select onChange={(e) => setAuthor(e.target.value)}>
-                    <option>Select Author</option>
-                    {authors.map(author => <option key={author.id} value={author.id}>{author.name}</option>)}
-                </select>
-            </div>
-            <button>Add</button>
-        </form>
-    )
-    return formular
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addBook({
+      variables: { name, genre, authorId: author },
+      refetchQueries: [{ query: getBooksQuery }]
+    })
+  }
+  const form = (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Book Name:</label>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Genre:</label>
+        <input type="text" onChange={(e) => setGenre(e.target.value)} />
+      </div>
+      <div>
+        <label>Author</label>
+        <select onChange={(e) => setAuthor(e.target.value)}>
+          <option>Select Author</option>
+          {authors.map((author) => (
+            <option key={author.id} value={author.id}>
+              {author.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button>Add</button>
+    </form>
+  )
+  return form
 }
 
 export default AddBook
---
+++ client/src/mutations/mutations.js
@@ -0,0 +1,11 @@
+import { gql } from 'apollo-boost'
+
+const ADD_BOOK = gql`
+  mutation($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
+      name
+      id
+    }
+  }
+`
+export { ADD_BOOK }
